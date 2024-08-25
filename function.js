e7a4543aa6d6e48198999cbba67dd47817ea3db3
Angha <anghavarangaonkar1104@gmail.com>
2024-08-25 20:41:18 +0530

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+// cant use try ,var , as a variable name 
+// declare a function 
+function user(name){
+    return 'hello ' +name+'!';
+}
+console.log(user('angha'));
+
+
+// function assigned to a variable 
+
+const ab = function(a,b){
+    return a+b;
+}
+
+console.log(ab(3,5));
