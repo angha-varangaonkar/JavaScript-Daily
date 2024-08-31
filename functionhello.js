106ac8d43786d25b0396b5e0062c1676c25a96a3
Angha <anghavarangaonkar1104@gmail.com>
2024-08-31 11:35:45 +0530

    daily commit

---
+++ functionhello.js
@@ -0,0 +1,12 @@
+// function greet(name){
+//     return ( name)
+// }
+//  console.log("hello "+greet("angha"));
+
+
+//  or
+
+function greet(name){
+    return ( "hello "+name)
+}
+ console.log(greet("angha"));
---
+++ helloworld.js
@@ -0,0 +1,4 @@
+console.log("hello woeld");
+
+// this will print in yellow as it is a string 
+console.log(345678)
