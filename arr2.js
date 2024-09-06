ed1851d65a492733acfdb98d60e74b7b00bc055d
Angha <anghavarangaonkar1104@gmail.com>
2024-09-06 13:11:07 +0530

    day6

---
+++ arr2.js
@@ -0,0 +1,7 @@
+// let arr =[2,8,3,3];
+// arr.push(23);
+// console.log(arr);
+
+arr=[4,'angha',7.9,true,[4,7,8,9]]
+console.log(arr);
+
---
+++ array.js
@@ -0,0 +1,19 @@
+arr=[1,2,3,8,6,7];
+console.log(arr);
+// n=arr.length;
+// console.log(n);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+arr.push(10);//add at last
+arr.push(0);
+console.log(arr);
+
+arr.pop(); //remove from ;last
+console.log(arr);
+arr.unshift(2); //add at first
+console.log(arr);
+arr.shift(); //remove from first
+console.log(arr);
